ca00f65d90d9db8c7cc67c3d6bd5a296b9a66db8
betulturkut <btlturkut@gmail.com>
2023-02-16 15:05:55 +0300

    Sum of numbers from 1 to 10
    1-10

--- LoopConsecutiveNumbers/Form1.Designer.cs
+++ LoopConsecutiveNumbers/Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(151, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonSum.Location = new System.Drawing.Point(74, 113);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(135, 71);
+            this.buttonSum.TabIndex = 1;
+            this.buttonSum.Text = "Hesapla";
+            this.buttonSum.UseVisualStyleBackColor = true;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(50, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sonuç:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(301, 238);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Button buttonSum;
+        private Label label2;
     }
 }
--- LoopConsecutiveNumbers/Form1.cs
+++ LoopConsecutiveNumbers/Form1.cs
@@ -6,5 +6,17 @@ namespace LoopConsecutiveNumbers
         {
             InitializeComponent();
         }
+
+        int toplam = 0;
+
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            for (int i = 0; i <= 10; i++)
+            {
+                toplam += i;
+            }
+            label1.Text = toplam.ToString();
+        }
     }
 }
