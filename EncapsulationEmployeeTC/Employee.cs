c53e30cb0999301d2e582622baf7c7ba7fb97467
betulturkut <btlturkut@gmail.com>
2023-02-16 16:59:35 +0300

    Encapsulation

---
+++ EncapsulationEmployeeTC/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationEmployeeTC
+{
+    public class Employee
+    {
+        private string TCNo;
+        public string TCNO
+        {
+            get
+            {
+                if (TCNo == null)
+                {
+                    return TCNo + "Hatalı!";
+                }
+                else
+                {
+                    return TCNo.Substring(0, 5) + "******";
+                }
+            }
+            set
+            {
+                bool control = false;
+
+                if (value.Length == 11)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool isNumber = char.IsNumber(value[i]); // Girilen karakter sayı mı? kontrolü yapılıyor.
+
+                        if (!isNumber)
+                        {
+                            control = true;
+                            break;
+                        }
+                    }
+
+                    if (control)
+                    {
+                        Console.WriteLine("Geçersiz karakter!");
+                    }
+                    else
+                    {
+                        TCNo = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz TC Kimlik No 11 karakter olmalı!");
+                }
+            }
+        }
+    }
+}
---
+++ EncapsulationEmployeeTC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Personelin TC Kimlik numarası alınırken yapılması gereken şartlar
+ 
+ * SET şartları
+ * 1- TC Kimlik no'su 11 karakter olmalı
+ * 2- Tüm karakterleri sayısal karakter olmalı
+ 
+ * GET şartları
+ * 1- TC Kimlik no'sunun sadece ilk 5 karakteri alınacak
+ 
+ */
+
+
+namespace EncapsulationEmployeeTC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.TCNO = "12345678920";
+            Console.WriteLine("TC Kimlik Numaranız: " + employee.TCNO);
+            Console.ReadLine();
+        }
+    }
+}
